4e0d4a29a177b0710ab9713c952947cde7a7a70a
Daniel Kubowicz <daniel.kubowicz10@gmail.com>
2026-09-10 16:50:20 +0200

    Naprawa tak aby działał po zmianach w API

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import os
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
-from librus_apix.client import Client, Token, new_client
-from librus_apix.student_information import get_student_information
+import requests
 from fastapi import FastAPI, Request
 
 load_dotenv()
 
+SYNERGIA_URL = "https://synergia.librus.pl"
+LUCKY_NUMBER_URL = f"{SYNERGIA_URL}/gateway/api/2.0/LuckyNumbers"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0",
+    "Content-Type": "application/x-www-form-urlencoded",
+}
+
+
+def login(login: str, password: str) -> requests.Session:
+    s = requests.Session()
+    s.headers.update(HEADERS)
+    s.headers["Referer"] = "https://portal.librus.pl/"
+
+    r = s.get(f"{SYNERGIA_URL}/loguj/portalRodzina", allow_redirects=True)
+    r = s.post(
+        r.url,
+        data={"action": "login", "login": login, "pass": password},
+    )
+    r = s.get(f"https://api.librus.pl{r.json()['goTo']}", allow_redirects=True)
+
+    return s
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    client: Client = new_client()
-    _token: Token = client.get_token(os.getenv("LOGIN"), os.getenv("PASSWORD"))
-    app.state.client = client
-    
+    app.state.session = login(os.getenv("LOGIN"), os.getenv("PASSWORD"))
     yield
-    
-    # tutaj możesz dodać cleanup jeśli biblioteka tego wymaga
 
 app = FastAPI(lifespan=lifespan)
 
+
 @app.get("/lucky-number")
 def lucky_number(request: Request):
-    info = get_student_information(request.app.state.client)
-    return {"lucky_number": info.lucky_number}
+    s: requests.Session = request.app.state.session
+    resp = s.get(LUCKY_NUMBER_URL)
+    data = resp.json()
+    return {"lucky_number": data["LuckyNumber"]["LuckyNumber"]}
